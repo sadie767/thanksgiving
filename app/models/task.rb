8a877bf618dfb3e99143cf97619116ad5626efba
Guest User <Guest@Epicodus-2B.local>
2017-11-14 16:40:41 -0800

    add scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,9 @@
 class Task < ActiveRecord::Base
   belongs_to :list
-
   validates :description, :presence => true
+  scope :today, -> { where("created_at >=?", Time.now.beginning_of_day) }
+  scope :ten_most_recent, -> { order(created_at: :desc).limit(10)}
+  def self.not_done
+    where(done: false)
+  end
 end
---
+++ db/migrate/20171115001822_add_column_to_tasks.rb
@@ -0,0 +1,6 @@
+class AddColumnToTasks < ActiveRecord::Migration[5.1]
+  def change
+    add_column(:tasks, :done, :boolean)
+
+  end
+end
